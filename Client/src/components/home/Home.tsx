ce7ad7ecdc1590664aaef3341fee32eeb4241040
elianbenjamin <elianbenjamin30@gmail.com>
2024-04-29 22:36:57 -0300

    commit

---
+++ Client/src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <img style={{height: '100vh', width: '100vw'}} src="../../assets/foto.png" alt="" />
+    </div>
+  )
+}
+
+export default Home
