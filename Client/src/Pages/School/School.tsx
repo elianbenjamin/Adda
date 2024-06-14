91a7f6b49f7d7848e3c77a7a91da07db8387f0da
RamiroPagella <ramiro.s.pagella@gmail.com>
2024-06-14 20:17:01 -0300

    nuevo estilo de card

--- Client/src/Pages/School/School.tsx
+++ Client/src/Pages/School/School.tsx
@@ -1,4 +1,5 @@
-import { DownloadIcon } from "../../assets/icons";
+import Career from "../../components/Career/Career";
+import Courses from "../../components/Courses/Courses";
 import style from "./school.module.scss";
 
 const School = () => {
@@ -7,27 +8,9 @@ const School = () => {
       <h1 className={style.title}>Cursos disponibles:</h1>
 
       <div className={style.cards}>
-        <section className={style.course1}>
-          <h1>CARRERA</h1>
-          <div className={style.content}>
-            <h3>Modelaje Profesional</h3>
+        <Career />
+        
 
-            <div>
-              <p>-1º año: Formacion integral del modelo.</p>
-              <p>-2º año: tecnica y modelaje internacional</p>
-              <p>-3º año: perfeccionamieto.</p>
-            </div>
-
-            <p>
-              Marzo a Noviembre. Todos los sabados de 14 a 17hs
-            </p>
-
-            <a>
-              Plan de estudio
-              <DownloadIcon/>
-            </a>
-          </div>
-        </section>
       </div>
     </div>
   );
---
+++ Client/src/components/Courses/Courses.tsx
@@ -0,0 +1,11 @@
+import style from './courses.module.scss';
+
+const Courses = () => {
+  return (
+    <div className={style.Courses}>
+      <h3>CURSOS</h3>
+    </div>
+  )
+}
+
+export default Courses
