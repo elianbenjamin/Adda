b046b72b610a85ffee8702a1d8e17a6c68064e59
RamiroPagella <ramiro.s.pagella@gmail.com>
2024-07-05 00:33:45 -0300

    toda la pagina totalmente responsive

--- Client/src/Pages/News/News.tsx
+++ Client/src/Pages/News/News.tsx
@@ -5,8 +5,50 @@ import { CalendarIcon, MapMarkerIcon } from "../../assets/icons";
 const News = () => {
   return (
     <div className={style.News}>
-      <section className={style.new1}>
-        <div className={style.contentContainer}>
+      <article>
+        <h1> !LA BODEGA EN PASARELA! </h1>
+
+        {/* <h3>
+          | Evento | <CalendarIcon /> 4 de Agosto | <MapMarkerIcon />
+          <a href="https://maps.app.goo.gl/ybqCBk9bYVFAgCUm8">
+            La bodega, Villa Nueva
+          </a>
+        </h3> */}
+
+        <div className={style.dataContainer}>
+          <h3>
+            <CalendarIcon /> 4 de Agosto{" "}
+          </h3>
+          <p>|</p>
+          <h3>
+            <MapMarkerIcon />
+            <a href="https://maps.app.goo.gl/ybqCBk9bYVFAgCUm8" target="_blank">
+              La bodega, Villa Nueva
+            </a>
+          </h3>
+        </div>
+
+        <div className={style.imgContainer}>
+          <img src={img} alt="Imagen de presentación" />
+        </div>
+
+        <p className={style.text}>
+          Estamos emocionados de anunciar el "Desfile en la Bodega de Villa
+          Nueva", un evento imperdible para los amantes de la moda, el sábado 15
+          de julio a las 7:00 PM. Participarán diseñadores nacionales e
+          internacionales, presentando colecciones desde alta costura hasta
+          streetwear vanguardista, con énfasis en la moda ética y sostenible.
+          <br />
+          <br />
+          La Bodega de Villa Nueva ofrecerá una experiencia única con
+          degustación de vinos locales y aperitivos gourmet. Los boletos son
+          limitados y están a la venta en nuestra página web. Para más
+          información, visita nuestro sitio web o contáctanos en redes sociales.
+          ¡Te esperamos!
+        </p>
+      </article>
+
+      {/* <div className={style.contentContainer}>
           <h1>¡LA BODEGA EN PASARELA!</h1>
 
           <div className={style.dateAndUbi}>
@@ -14,9 +56,7 @@ const News = () => {
               <CalendarIcon />
               <p>4 de Agosto</p>
             </div>
-
             -
-
             <div className={style.ubiContainer}>
               <MapMarkerIcon />
               <p>LA BODEGA, VILLA NUEVA</p>
@@ -41,8 +81,7 @@ const News = () => {
 
         <div className={style.imgContainer}>
           <img src={img} />
-        </div>
-      </section>
+        </div> */}
     </div>
   );
 };
